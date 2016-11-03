9cdd6c9ca2167cbd66b996140556d9041ca86243
Joel Stevick <joelstevick@outlook.com>
2016-11-03 14:32:49 -0400

    updates

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+﻿var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    autoprefixer = require('autoprefixer'),
+    postcss = require('gulp-postcss'),
+    cssvars = require('postcss-simple-vars'),
+    cssImport = require('postcss-import'),
+    nested = require('postcss-nested');
+
+gulp.task('default', function () {
+    console.log('gulp');
+});
+gulp.task('html', function () {
+    console.log('html task');
+});
+gulp.task('styles', function () {
+    return gulp.src('./app/assets/styles/styles.css')
+           .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+           .pipe(gulp.dest('./app/temp/styles'));
+});
+gulp.task('watch', function () {
+    watch('./app/index.html', function () {
+        gulp.start('html');
+    })
+    watch('./app/assets/styles/**/*.css', function () {
+        gulp.start('styles');
+    })
+});
